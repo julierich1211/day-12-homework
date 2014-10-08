5d7f2e9ce8658aeaa28b63175b2dd1acc21f36c3
Julie Rich <julierich1211@yahoo.com>
2014-10-08 16:49:21 -0500

    update

--- day12.js
+++ day12.js
@@ -9,57 +9,111 @@
 // //Use http://devdocs.io/javascript/global_objects/date for reference on creating Date() objects with a specific date.
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// set up arrays
-var numbers = [1,12,4,18,9,7,11,3,101,5,6];
-var strings = ['this','is','a','collection','of','words'];
-
-var customers = [
-    { firstname : 'Joe', lastname : 'Blogs'},
-    { firstname : 'John', lastname : 'Smith'},
-    { firstname : 'Dave', lastname : 'Jones'},
-    { firstname : 'Jack', lastname : 'White'}
-];
-
-console.log(customers);
-
-// YOUR CODE WILL GO IN THIS NEXT SECTION
-// find all customers whose firstname starts with 'J',
-// map() those people into an array of objects like: { name : c.firstname + " " + c.lastname }
-// then sort alphabetically
-
-var projections = customers
-    .filter(function(c){ /* ... */ })
-    .map(function(c){  /* ... */ })
-    .sort(sortByName);
-
-function sortByName(c1, c2) {
+function sumOf2(a, b) {
     "use strict";
-    //...
+    return a + b;
+}
+console.log(sumOf2(890, 987));
+
+function sumLoop() {
+    "use strict"
+    var arg = Array.prototype.slice.apply(arguments, []);
+    var sum = 0
+    for (var x = 0; x < arg.length; x++) {
+        sum += arg[x];
+    }
+    return sum;
+}
+console.log(sumLoop(2, 4, 6, 7, 8, 5, 4));
+
+
+function sumLoopAv() {
+    "use strict"
+    var arg = Array.prototype.slice.apply(arguments, []);
+    var sum = 0
+    for (var x = 0; x < arg.length; x++) {
+        sum += arg[x];
+    }
+    return (sum / arg.length);
+}
+console.log(sumLoopAv(2, 4, 6, 7, 8, 5, 4));
+
+
+function getMaxOfArguments() {
+    var par = 1 <= arguments.length ? [].slice.call(arguments, 0) : [];
+    return Math.max.apply(null, par);
+}
+console.log(getMaxOfArguments(1, 3, 9));
+
+
+//function longest(a) {
+//  var a = ["str1", "str2", "str3", "str4"];
+
+//function forEach(a, callback) {
+//  for (var i = 0; i < a.length; i++) {
+//    callback(a[i], i);
+//}
+
+// }
+//  Array.prototype.forEach = (callback) {
+//     for (var i = 0; i < this.length; i++) {
+//         callback(a[i], i);
+//     }
+//  }
+
+
+//  return current.length >= longest.length ? current : longest;
+//}
+//console.log(longest_substring('iillooovvvveeejjjjaaavvvvvaaaa'));
+
+
+function forEach(array, callback) {
+    for (var i = 0; i < array.length; i++) {
+        callback(array[i], i);
+    }
 }
 
-console.log(projections);
+function sortDateForLoop(dates) {
+    var newArray = [],
+        earliestDate;
+    for (var i = 0; i < dates.length; i++) {
+        earliestDate = dates[j];
+        // find lowest date and swap it with date
+        for (var j = i + 1; j < dates.length; j++) {
+
+            if (+dates[j] < +earliestDate) {
+                earliestDate = dates[j];
+                var temp = earliestDate;
+                earliestDate = dates[j];
+                dates[j] = temp;
+
+            }
+
+        }
+
+    }
+}
+
+
+function sortDateForEach(dates) {
+    dates.forEach(sortDateForLoop)
+
+}
+
+function sortDateCustomForEach(dates) {
+    forEach(dates, sortDateForLoop)
+}
+
+//setup
+var dates = [
+    new Date(2014, 9, 4),
+    new Date(2014, 9, 3),
+    new Date(2014, 8, 29),
+    new Date(2012, 9, 1),
+    new Date(2010, 2, 13)
+];
+
+console.log(dates);
+sortDateForLoop(dates);
+sortDateForEach(dates);
+sortDateCustomForEach(dates);
